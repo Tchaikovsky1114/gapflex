5943e3aa4b1994a5ff77c4392df9451184c08a32
Kim Myungseong <forwarm5891@gmail.com>
2022-05-01 05:48:10 +0900

    refactor: refact series.js.change public to static

--- js/series.js
+++ js/series.js
@@ -18,25 +18,27 @@ searchIcon.addEventListener('click', handleInput);
 
 //https://www.omdbapi.com/?apikey=7035c60c&t=frozen&type=series
 
-const movielist = document.querySelector('.movies--list');
-const API_KEY = 'apikey=7035c60c';
 
-
-const callMoreMovieButton = document.querySelector('.movies--button');
 
 async function getData(pageNumber, searchValue = 'bourne') {
   const { Search: movies, totalResults } = await (
     await fetch(`https://www.omdbapi.com?${API_KEY}&s=${searchValue}&page=${pageNumber}`)).json();
   renderMovies(movies, totalResults, searchValue);
+  if(pageNumber * 10 < totalResults){
   searchedResultCount(totalResults, searchValue);
+}else{
+  return;
+}
   console.log(totalResults);
+  
 }
 
 const renderMovies = (movies = 'bourne', totalResults, searchValue) => {
-  
+
   console.log(movies);
-  
+  console.log(pageNumber);
   [...movies].forEach((movie,i) => {
+    
     if(i < totalResults){
     const movieEl = document.createElement('li');
     const movieImg = document.createElement('img');
@@ -48,6 +50,7 @@ const renderMovies = (movies = 'bourne', totalResults, searchValue) => {
     `;
     movielist.append(movieEl);
     movieEl.append(movieImg);
+    
   }
   });
 };
@@ -58,19 +61,24 @@ const moviesCount = document.querySelector('.movies--count');
 const resultCount = document.createElement('div');
 
 const searchedResultCount = (totalResults, searchValue) => {
-  if(moviesCount.hasChildNodes()){
-    console.log('by returned');
-    return;
-  }
   resultCount.classList.add('movies--result')
+  if(moviesCount.hasChildNodes()){
+    resultCount.textContent = '';
+    resultCount.textContent = `There are "${totalResults}" total result for your "${searchValue.toUpperCase()}" search. `;
+  }
     resultCount.textContent = '';
     resultCount.textContent = `There are "${totalResults}" total result for your "${searchValue.toUpperCase()}" search. `;
     moviesCount.append(resultCount);
 
 };
 // 초기 제이슨 본 밀고 검색결과부터 보이게 하면 된다 미래의 나야
-const handleSearch = (e) => {
-  const searchResult = e.target.value;
+let searchResult;
+let pageNumber = 1;
+const handleSearch =  (e) => {
+  searchResult = e.target.value;
+  while(movielist.hasChildNodes()){
+    movielist.removeChild(movielist.firstChild)
+  }
   getData(1, searchResult);
   console.log('excuted');
 };
@@ -78,10 +86,12 @@ getData();
 
 searchInput.addEventListener('change', handleSearch);
 
-
-let pageNumber = 1;
-
-callMoreMovieButton.addEventListener('click', () => {
+const showMoreSearchedMovieResult = (searchResult) => {  
   pageNumber++;
-  getData(pageNumber);
-});
+  getData(pageNumber,searchResult);
+
+}
+callMoreMovieButton.addEventListener('click', () => showMoreSearchedMovieResult(searchResult));
+
+
+
